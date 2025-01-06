7ccd3f166131a8a3a347b8d61c3c7523c681d0ae
Wael Ahmed <waelsoliman@microsoft.com>
2025-01-06 18:46:14 +0200

    Added swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,17 @@
+import { INestApplication, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication, path: string): void {
+  const config = new DocumentBuilder()
+    .setTitle('Wedding Planner API')
+    .setDescription(
+      'API documentation for the NestJS Wedding Planner application',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(path, app, document);
+
+  Logger.log(`Swagger UI will be available at path: ${path}`);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  setupSwagger(app, 'api');
+
+  const port = process.env.PORT;
+  await app.listen(port);
+
+  if (process.env.NODE_ENV !== 'production') {
+    Logger.log(`Application is running on port http://localhost:${port}/`);
+  }
 }
 bootstrap();
