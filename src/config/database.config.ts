0c62381bb88b7264d005e29cade6f5e079d4080c
Wael Ahmed <waelsoliman@microsoft.com>
2025-01-06 18:46:07 +0200

    Correcting env variable name

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export const databaseConfig = (): TypeOrmModuleOptions => ({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   autoLoadEntities: true,
   synchronize: process.env.NODE_ENV !== 'production',
